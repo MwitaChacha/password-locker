d1de6aa5bfdba6d8d853d19e3ebb8652a6069593
Mwita Chacha <jonahhchacha@gmail.com>
2021-10-24 06:27:11 +0300

    Creating the constructor for the Credentials class

--- credentials.py
+++ credentials.py
@@ -1,2 +1,15 @@
 class Credentials:
     
+    """
+    A class that generates new instances of creentials
+    """
+    
+    credentials_list = []
+    
+    def __init__(self, account_name, account_password):
+        """
+        A method that defines properties of the credentials class 
+        """
+        self.account_name = account_name
+        self.account_password = account_password
+        
