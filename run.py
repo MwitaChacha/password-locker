7416c8a8eb654828cd60da70aefee940e0c6be90
Mwita Chacha <jonahhchacha@gmail.com>
2021-10-24 06:45:39 +0300

    Creating a function that creates new users

--- run.py
+++ run.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3.9
 import random
 from user import User
-from credentials import credentials
+from credentials import Credentials
 
+def create_user(username, password):
+    """[A function that creates a new user]
+
+    Args:
+        username ([type]): [description]
+        password ([type]): [description]
+    """
+    new_user = User(username, password)
+    return new_user
 
